184b7dab77061cc04e203e2aceb3545e07eda097
Pavle Konakovic <pavle.konakovic@gmail.com>
2022-11-18 17:52:18 +0100

    Exercises 4.15 - 4.16

--- part4/blog-list/tests/blogsForTest.js
+++ part4/blog-list/tests/blogsForTest.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const emptyBlog = []
 
@@ -69,6 +70,11 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
-  emptyBlog, listWithOneBlog, blogs, blogsInDb
+  emptyBlog, listWithOneBlog, blogs, blogsInDb, usersInDb
 }
