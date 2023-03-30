cb8b9328d8ddb126cb43d9a9bd2595365ebcc7e8
Pavle Konakovic <pavle.konakovic@gmail.com>
2023-03-30 17:53:28 +0200

    Exercises 8.17 - 8.18

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,37 +1,61 @@
-import { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
-import Notify from './components/Notify'
-
+import { useApolloClient } from "@apollo/client";
+import { useState } from "react";
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+import Notify from "./components/Notify";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
-  const [page, setPage] = useState('authors')
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [page, setPage] = useState("authors");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [token, setToken] = useState(null);
+  const client = useApolloClient();
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   const notify = (message) => {
-    setErrorMessage(message)
+    setErrorMessage(message);
     setTimeout(() => {
-      setErrorMessage(null)
-    }, 5000)
-  }
+      setErrorMessage(null);
+    }, 5000);
+  };
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        {token ? (
+          <>
+            <button onClick={() => setPage("add")}>add book</button>
+            <button onClick={logout}>logout</button>
+          </>
+        ) : (
+          <button onClick={() => setPage("login")}>login</button>
+        )}
       </div>
       <Notify errorMessage={errorMessage} />
 
-      <Authors show={page === 'authors'} setError={notify} />
+      <Authors show={page === "authors"} setError={notify} />
 
-      <Books show={page === 'books'} />
+      <Books show={page === "books"} />
 
-      <NewBook show={page === 'add'} setError={notify} />
+      <NewBook show={page === "add"} setError={notify} />
+
+      <LoginForm
+        show={page === "login"}
+        setToken={setToken}
+        setError={notify}
+        setPage={setPage}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -13,7 +13,9 @@ export const ALL_BOOKS = gql`
   query{
     allBooks{
       title
-      author
+      author {
+        name
+      }
       published
     }
   }
@@ -40,6 +42,14 @@ export const EDIT_AUTHOR = gql`
     editAuthor(name: $name, setBornTo: $setBornTo) {
       name
       born
+    }
+  }
+`
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password){
+      value
     }
   }
 `
